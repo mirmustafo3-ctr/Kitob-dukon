afe1778f8f764c5e41b59a5f2a3bf4b2a6c67cfe
mirmustafo3-ctr <mirmustafo3@gmail.com>
2026-06-24 11:33:31 +0500

    edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
           <p>Sizning sevimli onlayn kitob do'koningiz. Eng sara asarlar va hamyonbop narxlar faqat bizda.</p>
           
           <div className="social-icons">
-            <a href=""><i className="fa-brands fa-telegram"></i></a>
-            <a href=""><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://t.me/mustafomirzayunusov"><i className="fa-brands fa-telegram"></i></a>
+            <a href="https://www.instagram.com/mustafo.__000"><i className="fa-brands fa-instagram"></i></a>
             <a href=""><i className="fa-brands fa-facebook"></i></a>
-            <a href=""><i className="fa-brands fa-youtube"></i></a>
+            <a href="https://www.youtube.com/feed/you"><i className="fa-brands fa-youtube"></i></a>
           </div>
         </div>
 
@@ -30,7 +30,7 @@ function Footer() {
 
         <div className="footer-contact">
           <h3>Aloqa</h3>
-          <p><i className="fa-solid fa-phone"></i> +998 (90) 123-45-67</p>
+          <p><i className="fa-solid fa-phone"></i> +998 (91) 155-77-25</p>
           <p><i className="fa-solid fa-envelope"></i> info@bookstore.uz</p>
           <p><i className="fa-solid fa-location-dot"></i> Toshkent sh., Chilonzor tumani</p>
         </div>
